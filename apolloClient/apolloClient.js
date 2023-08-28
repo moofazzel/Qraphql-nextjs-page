17a99db58119d008585a03bf91c82d6a76452e56
moofazzel <2192461016@uttarauniversity.edu.bd>
2023-08-28 21:32:37 +0600

    added uri env variable in alolloclient

--- apolloClient/apolloClient.js
+++ apolloClient/apolloClient.js
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
-    link: new HttpLink({ uri: '/api/graphql', credentials: 'same-origin' }),
+    link: new HttpLink({ uri: process.env.NEXT_PUBLIC_GRAPHQL_API, credentials: 'same-origin' }),
     cache: new InMemoryCache(),
 });
 
